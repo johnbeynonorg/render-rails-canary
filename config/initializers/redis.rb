5c947460269f44672af44664a8a9a79212875a0c
John Beynon <john@render.com>
2021-07-23 12:47:51 +0000

    PORT NOT URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-REDIS_URL = "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_URL']}"
+REDIS_URL = "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
 
 Sidekiq.configure_server do |config|
 	config.redis = { url: REDIS_URL }
