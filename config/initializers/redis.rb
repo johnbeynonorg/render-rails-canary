412d58d72a9ca976c3abec59acc3946de7ab0428
John Beynon <john@render.com>
2021-07-23 12:24:05 +0000

    Simplify

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-REDIS_URL = "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_URL']}" | ENV['REDIS_URL']
+REDIS_URL = "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_URL']}"
 
 Sidekiq.configure_server do |config|
 	config.redis = { url: REDIS_URL }
