29047c6956671b8689792d5fe640ef36b04b4fcf
John Beynon <john@render.com>
2021-07-23 11:29:16 +0000

    initial commit

---
+++ bin/devcontainer-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+yarn install
+bundle exec rake assets:precompile
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
