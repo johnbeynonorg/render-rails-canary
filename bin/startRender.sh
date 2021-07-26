75e1b429c87a091dffc1529eedc3cf4f35c10131
John Beynon <john@render.com>
2021-07-26 18:02:51 +0100

    Syntax

--- bin/startRender.sh
+++ bin/startRender.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-export REDIS_URL="redis://${REDIS_HOST}:${REDIS_PORT}"
-bundle exec puma -C config/puma.rb
+REDIS_URL="redis://${REDIS_HOST}:${REDIS_PORT}" bundle exec puma -C config/puma.rb
