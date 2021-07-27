ae12e4824582ee596fa973ddc4ec49a7020d2507
John Beynon <john@render.com>
2021-07-27 12:19:03 +0100

    Clean up

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,3 @@
-ENV['REDIS_URL'] = "redis://#{ENV['REDIS_HOSTPORT']}"
-
-$redis = Redis.new(url: ENV['REDIS_URL'])
-
-Sidekiq.configure_server do |config|
-      config.redis = { url: ENV['REDIS_URL'] }
-end
-
-Sidekiq.configure_client do |config|
-      config.redis = { url: ENV['REDIS_URL'] }
+unless ENV.has_key?('REDIS_URL')
+      ENV['REDIS_URL'] = "redis://#{ENV['REDIS_HOSTPORT']}"
 end
